0f3c14cf52e3f3f0f7181d44ac3efae5b542506c
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-04-11 20:08:25 -0600

    Add comments and general polish

--- JobApplicationTracker/JobForm.cs
+++ JobApplicationTracker/JobForm.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
-using System.Reflection;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace JobApplicationTracker
 {
@@ -21,106 +16,24 @@ namespace JobApplicationTracker
             InitializeComponent();
         }
 
-        /*Excel.Application xlApp = new Excel.Application();
-        Excel.Workbook xlWb;
-        Excel.Worksheet xlWs;
-
-        private void ReadExcel(string wbName, DataGridView dgv)
-        {
-            xlWb = xlApp.Workbooks.Open(wbName);
-            xlWs = xlWb.Worksheets["Sheet1"];
-
-            for (int iCol = 1; iCol <= xlWs.Columns.Count; iCol++)
-            {
-                if (xlWs.Cells[1, iCol].value == null)
-                {
-                    break;
-                }
-                else
-                {
-                    DataGridViewColumn col = new DataGridViewTextBoxColumn();
-                    col.HeaderText = xlWs.Cells[1, iCol].Value;
-                    dgv.Columns.Add(col);
-                }
-            }
-
-            for (int iRow = 2; iRow <= xlWs.Rows.Count; iRow++)
-            {
-                if (xlWs.Cells[iRow, 1].value == null)
-                {
-                    break;
-                }
-                else
-                {
-                    string[] row = new string[] {
-                        xlWs.Cells[iRow, 1].Value,
-                        xlWs.Cells[iRow, 2].Value,
-                        xlWs.Cells[iRow, 3].Value,
-                        xlWs.Cells[iRow, 4].Value,
-                    };
-                    
-                    dgv.Rows.Add(row);
-                }
-            }
-
-            xlWb.Close();
-            xlApp.Quit();
-
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlApp);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWb);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlWs);
-        }
-
-        private void SaveToExcel(DataGridView dgv, string xlWb)
-        {
-            StreamWriter wr = new StreamWriter(xlWb);
-
-            int cols = dgv.Columns.Count;
-        }*/
-
-        /*[Serializable()]
-        public class Job
-        {
-            public int IdNumber { get; set; }
-            public string PositionTitle { get; set; }
-            public string Employer { get; set; }
-            public string FtPt { get; set; }
-            public string EmpCon { get; set; }
-            public bool Applied { get; set; }
-
-            public Job(
-                int idNumber,
-                string positionTitle, 
-                string employer, 
-                string ftPt, 
-                string empCon,
-                bool applied
-                )
-            {
-                IdNumber = idNumber;
-                PositionTitle = positionTitle;
-                Employer = employer;
-                FtPt = ftPt;
-                EmpCon = empCon;
-                Applied = applied;
-            }
-
-            public Job() { }
-        }*/
-
         BindingList<Job> jobsBindingList = new BindingList<Job>();
         List<int> valuesList;
         BindingSource jobBindingSource;
+
         string JobsFileName = Application.StartupPath 
             + @"\JobData.bin";
         string ValuesFileName = Application.StartupPath
             + @"\ValuesData.bin";
         string AppDatesFileName = Application.StartupPath
             + @"\AppDatesData.bin";
+
         public static int ftPtSurveyTrackBarValue;
         public static int empConSurveyTrackBarValue;
         string ftPtFilterValue;
         string empConFilterValue;
+
+        // Stored as the job's unique ID as the key and the datatime applied
+        // as the value
         Dictionary<int, DateTime> jobApplicationDates =
             new Dictionary<int, DateTime>();
 
@@ -152,13 +65,6 @@ namespace JobApplicationTracker
         
         private void JobForm_Load(object sender, EventArgs e)
         {
-            // BindingSource jobBindingSource = new BindingSource();
-
-            // jobDataGridView.DataSource = jobBindingSource;
-
-            // FileStream openFileStream;
-            // BinaryFormatter deserializer = new BinaryFormatter();
-
             if (File.Exists(JobsFileName))
             {
                 GetJobsBindingList();
@@ -262,6 +168,7 @@ namespace JobApplicationTracker
             saveFileStream.Close();
         }
 
+        // USed internally for scoring jobs for recommendation purposes
         private int IncrementJobScore(DataGridViewRow job, string filterValue,
             int cellIndex, int trackBarValue)
         {
